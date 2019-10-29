47c542b0eff1ef8a835b78cc63ac0040992ac0f4
EulerGiovanini <57120222+EulerGiovanini@users.noreply.github.com>
2019-10-28 22:28:24 -0300

    Adicionei três pastas da aula 01
    Conteúdo sobre alert, form, e evento onclick

---
+++ funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Exemplo de uma funcção simples
+ * @author Euler Aguiar Giovanini
+ */
+
+ function helloWorld(){
+     let nome
+     nome = prompt('Qual é o seu nome')
+     alert ('Hello '+ nome)
+ }
---
+++ vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * Exemplo de vinculando de um documento JS no html
+ * @author Euler Aguiar Giovanini
+ */
+
+ document.write('<h2>'+'Hello World'+'</h2>')
